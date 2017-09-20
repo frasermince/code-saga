60f68caf9322c3316dc230718cff46276b3229c1
Fraser Mince <frasermince@gmail.com>
2017-09-20 14:14:59 -0500

    Update content for mal presentation.

--- src/App/Presentations/MalPresentation.purs
+++ src/App/Presentations/MalPresentation.purs
@@ -4,6 +4,84 @@ import Prelude
 import App.Prelude
 import App.State (SlideData(..), BeforeOrAfter(..))
 
+types="\
+\{-# LANGUAGE TypeSynonymInstances #-}\n\
+\{-# LANGUAGE FlexibleInstances #-}\n\
+\module Types\n\
+\( Sexp(..)\n\
+\, Environment(..)\n\
+\, Command(..)\n\
+\, AppliedCommand(..)\n\
+\, Bindings(..)\n\
+\, RunTimeError(..)\n\
+\, Eval(..)\n\
+\, MalError(..)\n\
+\, runEval\n\
+\, runEvalForTuple\n\
+\) where\n\
+\import qualified Data.Map as Map\n\
+\import Control.Monad.Writer.Lazy\n\
+\import Control.Monad.Except\n\
+\import Control.Monad.Identity\n\
+\import Text.Parsec\n\
+\type Params = [Sexp]\n\
+\type Bindings = [Sexp]\n\
+\type Body = Sexp\n\
+\type RunTimeError = String\n\
+\\n\
+\type Environment = [Map.Map String Sexp]\n\
+\type Eval = WriterT Environment (ExceptT MalError Identity) Sexp\n\
+\runEval :: Eval -> Either MalError Sexp\n\
+\runEval eval = runIdentity $ runExceptT result\n\
+\  where result = fst <$> runWriterT eval\n\
+\\n\
+\runEvalForTuple :: Eval -> Either MalError (Sexp, Environment)\n\
+\runEvalForTuple eval = runIdentity $ runExceptT $ runWriterT eval\n\
+\\n\
+\type AppliedCommand = Bindings -> Environment -> Eval\n\
+\type Command = Params -> Body -> AppliedCommand\n\
+\\n\
+\data Sexp = MalNum Int | MalSymbol String | MalList [Sexp] | MalFunction AppliedCommand | MalBool Bool | MalString String\n\
+\            deriving (Eq)\n\
+\\n\
+\data MalError = MalParseError ParseError | MalEvalError String\n\
+\\n\
+\instance Show Sexp where\n\
+\  show (MalNum x) = show x\n\
+\  show (MalSymbol x) = x\n\
+\  show (MalBool x) = show x\n\
+\  show (MalString string) = \"\\\"\" ++ string ++ \"\\\"\"\n\
+\  show (MalFunction appliedCommand) = \"<FN>\"\n\
+\  show (MalList sexps) = \"(\" ++ foldl convertToString \"\" sexps ++ \")\"\n\
+\\n\
+\    where\n\
+\      convertToString \"\" sexp = show sexp\n\
+\      convertToString accumulator sexp = accumulator ++ \" \" ++ show sexp\n\
+\\n\
+\instance {-# OVERLAPS #-} Monoid Environment where\n\
+\  mempty = []\n\
+\  mappend x y = reverse $ mergeLists xList yList\n\
+\    where xList = reverse x\n\
+\          yList = reverse y\n\
+\          mergeLists x [] = x\n\
+\          mergeLists [] y = y\n\
+\          mergeLists (x:xs) (y:ys) = (y `Map.union` x) : mergeLists xs ys\n\
+\\n\
+\instance Eq MalError where\n\
+\  MalParseError one == MalParseError two = one == two\n\
+\  MalEvalError one == MalEvalError two = one == two\n\
+\\n\
+\instance Show MalError where\n\
+\  show (MalParseError error) = show error\n\
+\  show (MalEvalError error) = show error\n\
+\\n\
+\instance Eq AppliedCommand where\n\
+\  commandOne == commandTwo = runEval (commandOne bindings mempty ) == runEval (commandTwo bindings mempty)\n\
+\    where bindings = [MalNum 1, MalNum 2]\n\
+\\n\
+\--instance Show Eval where\n\
+\  --show eval = show $ runEval eval"
+
 entry = "\
 \module Main where\n\
 \\n\
@@ -88,7 +166,57 @@ presentation =
       , lineNumber: 7
       , language: "haskell"
       , beforeOrAfter: Nothing
-      , annotation: "This function is the entry point into the src portion of my program. While main creates an executable, this creates a library that is used both in the executable and in my test suite."
+      , annotation: "This function is the entry point into the src portion of my program. While main creates an executable, this creates a library that is used both in the executable and in the test suite."
+      , content: rep
+      }
+
+  , SlideData
+      { fileName: "ReadEval.hs"
+      , filePath: "src/"
+      , lineNumber: 3
+      , language: "haskell"
+      , beforeOrAfter: Nothing
+      , annotation: "As you can see I have defined several types here. They are all found in my Types module so let's head over there now!"
+      , content: rep
+      }
+
+  , SlideData
+      { fileName: "Types.hs"
+      , filePath: "src/"
+      , lineNumber: 37
+      , language: "haskell"
+      , beforeOrAfter: Nothing
+      , annotation: "A Sexp is an S expression. This is a lisp expression that is made up of some Lisp type that we store as data constructors. It is recursive so it can hold a list of other Sexps."
+      , content: types
+      }
+
+  , SlideData
+      { fileName: "Types.hs"
+      , filePath: "src/"
+      , lineNumber: 25
+      , language: "haskell"
+      , beforeOrAfter: Nothing
+      , annotation: "The environment is a list of maps that have string keys and sexp values. This is the datastructure we use to get the values of variables. It's in an array so we can have different layers of environment depending on the scope."
+      , content: types
+      }
+
+  , SlideData
+      { fileName: "Types.hs"
+      , filePath: "src/"
+      , lineNumber: 40
+      , language: "haskell"
+      , beforeOrAfter: Nothing
+      , annotation: "The errors are either a parse error or a eval error. These are errors from the respective read and eval steps of the REPL process."
+      , content: types
+      }
+
+  , SlideData
+      { fileName: "ReadEval.hs"
+      , filePath: "src/"
+      , lineNumber: 6
+      , language: "haskell"
+      , beforeOrAfter: Nothing
+      , annotation: "Now that we've looked at the types let's look at this signature again! This function takes an environment (which in practice is a default or the result of a previous command) and the actual lisp code. It then returns either an error or a tuple with the environment and result."
       , content: rep
       }
   ]
