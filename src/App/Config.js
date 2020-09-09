12cc3d803c7eedcb4b76125023c8b09d37067fd2
Fraser Mince <fraser@thoughtbot.com>
2020-09-09 16:26:33 -0500

    Remove environment switch for public path.

--- src/App/Config.js
+++ src/App/Config.js
@@ -2,5 +2,7 @@
 
 exports.config = {
   title: 'CodeSaga',
-  public_path: process.env.NODE_ENV === 'production' ? '/dist' : 'http://localhost:8080/static/dist'
+  public_path: '/dist'
 };
+//# sourceMappingURL=Config.js.map
+//# sourceMappingURL=Config.js.map
